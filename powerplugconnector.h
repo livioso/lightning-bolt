6047b1553abd146a15979e7e2819384411afc1d1
Livio Bieri <livio@livio.li>
2014-01-23 09:43:47 +0100

    Replaced defines with constants in dedicated GPIO struct

--- powerplugconnector.h
+++ powerplugconnector.h
@@ -2,17 +2,40 @@
 #include <map>
 #include <string>
 
-class PowerPlugConnector {
-	enum EPowerPlugSerialnumber {
-		ePowerPlugOne = 744102883,
-		ePowerPlugTwo = 424110073
-	};
+// GPIO setup macros. Always use INP_GPIO
+// before using OUT_GPIO or SET_GPIO_ALT
 
-	std::map< EPowerPlugSerialnumber, std::string > mPowerPlugsCollection;
+/*
+	#define INP_GPIO(g) *(gpio + ((g)/10)) &= ~(7<<(((g)%10)*3))
+	#define OUT_GPIO(g) *(gpio + ((g)/10)) |= (1<<(((g)%10)*3))
+	#define SET_GPIO_ALT(g,a) *(gpio + (((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+	#define GPIO_SET *(gpio + 7)
+	#define GPIO_CLR *(gpio + 10)
+*/
+
+class PowerPlugConnector {
+
+	private:
+
+		struct GPIO {
+			const unsigned int kPageSize = 4096;
+			const unsigned int kBlockSize = 4096;
+			const unsigned int kBCM2708PeriBase = 0x20000000;
+			const unsigned int kGPIOControllerBase = kBCM2708PeriBase + 0x200000;
+			volatile unsigned* gpio;
+		} GPIO; 
+
+		enum EPowerPlugSerialnumber {
+			ePowerPlugOne = 744102883,
+			ePowerPlugTwo = 424110073
+		};
+
+		std::map< EPowerPlugSerialnumber, std::string > mPowerPlugsCollection;
 
 	public:
-		explicit PowerPlugConnector ();
-		void setupPowerPlugs ();
-		void turnPowerPlugOn (EPowerPlugSerialnumber id);
-		void turnPowerPlugOff (EPowerPlugSerialnumber id);
+			explicit PowerPlugConnector ();
+			void setupPowerPlugs ();
+			void turnPowerPlugOn (EPowerPlugSerialnumber id);
+			void turnPowerPlugOff (EPowerPlugSerialnumber id);
 };
